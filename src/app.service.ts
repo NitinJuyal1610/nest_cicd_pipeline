29eb6bb69c4a0f4d3169eb5a34a68d71f914d885
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-12-25 11:34:40 +0530

    env check

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const configService = new ConfigService();
+    console.log(configService.get('RDS_HOSTNAME'), '-IS MY VAL');
     return 'Hello World!';
   }
 }
