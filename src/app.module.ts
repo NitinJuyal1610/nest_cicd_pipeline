bac85eadcf2aa9fb0fdae50e2d9592b2301bd453
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-12-25 12:43:37 +0530

    env update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'database-2.ctcc6oc4284s.ap-south-1.rds.amazonaws.com',
-      port: 5432,
-      username: 'postgres',
-      password: 'nitinjuyal1610',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('RDS_HOSTNAME'),
+        port: 3000,
+        username: configService.get('RDS_USERNAME'),
+        password: configService.get('RDS_PASSWORD'),
+        database: configService.get('RDS_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    const configService = new ConfigService();
-    console.log(configService.get('RDS_HOSTNAME'), '-IS MY VAL');
     return 'Hello World!';
   }
 }
